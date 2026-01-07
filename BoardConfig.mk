f1bcaaa71b48f17555bd8ca3d48c8cbd6a4f86ab
Aeoniixx <aeoniixx@gmail.com>
2026-01-07 07:59:11 +0000

    lavender: Build Camera HAL from source
    Signed-off-by: Aeoniixx <aeoniixx@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,7 +23,7 @@ TARGET_TS_MAKEUP := true
 TARGET_USES_QTI_CAMERA_DEVICE := true
 BOARD_QTI_CAMERA_32BIT_ONLY := true
 USE_DEVICE_SPECIFIC_CAMERA := true
-$(call project-set-path,qcom-camera,$(DEVICE_PATH)/camera)
+$(call project-set-path,qcom-camera,hardware/qcom-caf/sdm660/camera)
 
 # Device Path
 DEVICE_PATH := device/xiaomi/lavender
